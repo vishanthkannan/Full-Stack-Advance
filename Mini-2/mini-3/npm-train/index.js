bbdf0f3aec5b32aae01d4332b16dcce9bf172194
vishanth <vishanthkannan777@gmail.com>
2025-12-09 16:05:03 +0530

    Added Express.js CRUD API project

--- Mini-2/mini-3/npm-train/index.js
+++ Mini-2/mini-3/npm-train/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
-});
+// Start server
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+
+ app.use(express.json());
+ const userRouter = require("./routes/userRoutes");
+
+ app.use("/users", userRouter);
+
+ app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
